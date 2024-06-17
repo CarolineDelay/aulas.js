aca1355f00bf1b5fb0c12ec8ca09bf04af2d8ee6
Caroline Delay <146271511+CarolineDelay@users.noreply.github.com>
2024-06-17 19:58:17 -0300

    aula 1 e 2 de JS

---
+++ aula2/aula2.js
@@ -0,0 +1,5 @@
+document.getElementById("texto").innerHTML = "meu primeiro texto em JS usando o document get element by id";
+
+//* alert(5+5);
+
+console.log('Oi, isso é feito usando o console.log')
---
+++ js/index.js
@@ -0,0 +1,2 @@
+
+    alert ('Um alerta para exemplificar que ele roda em uma página externa');
