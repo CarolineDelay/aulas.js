e16870d6a57f65a6cc0061a736a39a648b8a13a1
Caroline Delay <146271511+CarolineDelay@users.noreply.github.com>
2024-06-18 21:55:18 -0300

    aulas 3,4,5 e 6

--- aula2/aula2.js
+++ aula2/aula2.js
@@ -3,3 +3,20 @@ document.getElementById("texto").innerHTML = "meu primeiro texto em JS usando o 
 //* alert(5+5);
 
 console.log('Oi, isso é feito usando o console.log')
+
+//*alert('olá, só para relembrar o alerta');
+
+document.getElementById("teste").innerHTML = "olá, pessoal, isso foi somente para relembrar meus estudos";
+
+let conta = "a, b, c";
+a = 5;
+b = 2;
+c= 2;
+
+let nome="carol";
+let sobrenome="delay";
+
+console.log(nome+sobrenome);
+
+var pote = "bombom";
+alert(pote);
